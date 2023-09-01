991f70774570bf6a3af275bd49253546d6326b26
ankit-suman-07 <ankitsuman07@gmail.com>
2023-09-02 03:22:38 +0530

    Added graph and pie chart

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import "../css/Graph.css";
+import { Chart } from "react-google-charts";
+
+export const data = [
+  ["Year", "Sales", "Expenses"],
+  ["2004", 1000, 400],
+  ["2005", 1170, 460],
+  ["2006", 660, 1120],
+  ["2007", 1030, 540],
+];
+
+export const options = {
+  title: "Company Performance",
+  curveType: "function",
+  legend: { position: "bottom" },
+};
 
 export const Graph = () => {
   return (
-    <div className='graph-comp' >
-      Graph
+    <div className='chart-div' >
+      <Chart
+      chartType="LineChart"
+      width="100%"
+      height="240px"
+      data={data}
+      options={options}
+    />
     </div>
-  )
+    
+  );
 }
--- src/components/PieChart.jsx
+++ src/components/PieChart.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
 import "../css/PieChart.css";
 
+import CanvasJSReact from '@canvasjs/react-charts';
+
+const CanvasJS = CanvasJSReact.CanvasJS;
+const CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
 export const PieChart = () => {
+  const options = {
+    // theme: 'dark2',
+    animationEnabled: true,
+    // exportFileName: "New Year Resolutions",
+    // exportEnabled: true,
+    title: {
+      text: "Top Products"
+    },
+    data: [{
+      type: "pie",
+      showInLegend: true,
+      legendText: "{label}",
+      toolTipContent: "{label}: <strong>{y}%</strong>",
+      indexLabel: "{y}%",
+      indexLabelPlacement: "inside",
+      dataPoints: [
+        { y: 31, label: "Basic Tees" },
+        { y: 55, label: "Custom Short Pants" },
+        { y: 14, label: "Super Hoodies" }
+      ]
+    }]
+  };
+
   return (
     <div className='pie-chart-comp' >
-      Pie Chart
+      <CanvasJSChart options={options} />
     </div>
-  )
+  );
 }
