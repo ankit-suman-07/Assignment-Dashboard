89a35b8724005ac4997476ab2c11f11065d9b3d6
ankit-suman-07 <ankitsuman07@gmail.com>
2023-09-02 04:29:05 +0530

    Added line graph

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -3,26 +3,46 @@ import "../css/Graph.css";
 import { Chart } from "react-google-charts";
 
 export const data = [
-  ["Year", "Sales", "Expenses"],
-  ["2004", 1000, 400],
-  ["2005", 1170, 460],
-  ["2006", 660, 1120],
-  ["2007", 1030, 540],
+  ["Week", "Value 1", "Value 2"],
+  ["Week 1", 390, 400],
+  ["Week 2", 150, 220],
+  ["Week 3", 540, 350],
+  ["Week 4", 120, 220],
 ];
 
+
 export const options = {
-  title: "Company Performance",
+  
   curveType: "function",
-  legend: { position: "bottom" },
+  legend: "none",
+  colors: ["#E9A0A0", "#9BDD7C"],
+  hAxis: {
+    gridlines: {
+      count: data.length - 1, // Number of vertical gridlines (one less than data points)
+      color: "lightgray", // Color of gridlines
+    },
+  },
 };
 
 export const Graph = () => {
   return (
     <div className='chart-div' >
+      <div className='chart-head-div' >
+        Activities
+      </div>
+      <div className='graph-legends-div' >
+        Sort 
+        <div className='graph-legends' >
+          <div className='circle pink' ></div>
+          Guest
+          <div className='circle green' ></div>
+          Users
+        </div>
+      </div>
       <Chart
       chartType="LineChart"
       width="100%"
-      height="240px"
+      height="180px"
       data={data}
       options={options}
     />
--- src/components/PieChart.jsx
+++ src/components/PieChart.jsx
@@ -1,38 +1,13 @@
 import React from 'react';
 import "../css/PieChart.css";
 
-import CanvasJSReact from '@canvasjs/react-charts';
-
-const CanvasJS = CanvasJSReact.CanvasJS;
-const CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 export const PieChart = () => {
-  const options = {
-    // theme: 'dark2',
-    animationEnabled: true,
-    // exportFileName: "New Year Resolutions",
-    // exportEnabled: true,
-    title: {
-      text: "Top Products"
-    },
-    data: [{
-      type: "pie",
-      showInLegend: true,
-      legendText: "{label}",
-      toolTipContent: "{label}: <strong>{y}%</strong>",
-      indexLabel: "{y}%",
-      indexLabelPlacement: "inside",
-      dataPoints: [
-        { y: 31, label: "Basic Tees" },
-        { y: 55, label: "Custom Short Pants" },
-        { y: 14, label: "Super Hoodies" }
-      ]
-    }]
-  };
+  
 
   return (
     <div className='pie-chart-comp' >
-      <CanvasJSChart options={options} />
+      Pie Chart
     </div>
   );
 }
