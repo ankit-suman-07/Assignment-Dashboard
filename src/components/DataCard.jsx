3a04a9b7dd206af9f4aea26d75eab6e5d1bf5e17
ankit-suman-07 <ankitsuman07@gmail.com>
2023-09-02 02:13:21 +0530

    Updated DataCard css

--- src/components/DataCard.jsx
+++ src/components/DataCard.jsx
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
 import "../css/DataCard.css";
+import Transaction from "../icons/transaction.png";
 
 export const DataCard = () => {
   const val = 12345;
   return (
     <div className='datacard' >
-        Title
+      <div className='datacard-image-div' >
+        <img src={Transaction} />
+      </div>
+      <span className='datacard-head-div' >
+        Total Revenue
+      </span>
+      <span className='datacard-val-div' >
         {val}
-        Icon
+      </span>
     </div>
   )
 }
