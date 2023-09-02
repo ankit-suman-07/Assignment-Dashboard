74716c28d6940153d3b79c6454b10d8633403041
ankit-suman-07 <ankitsuman07@gmail.com>
2023-09-02 09:38:04 +0530

    Modified Schedule card

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -4,7 +4,17 @@ import "../css/Schedule.css";
 export const Schedule = () => {
   return (
     <div className='schedule-comp' >
-      Schedule
+      <span className='schedule-title-div' >Today's Schedule</span>
+      <div className='schedule-task-1' >
+        <span className='schedule-name' >Schedule Name</span>
+        <span className='schedule-time' >Schedule Name</span>
+        <span className='schedule-location' >Schedule Name</span>
+      </div>
+      <div className='schedule-task-2' >
+        <span className='schedule-name' >Schedule Name</span>
+        <span className='schedule-time' >Schedule Name</span>
+        <span className='schedule-location' >Schedule Name</span>
+      </div>
     </div>
   )
 }
