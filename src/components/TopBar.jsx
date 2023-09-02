df6a36b1137cdff25f776b5013ff2e781634eb52
ankit-suman-07 <ankitsuman07@gmail.com>
2023-09-02 06:59:27 +0530

    Added icones, modified topbar

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -4,9 +4,15 @@ import "../css/TopBar.css";
 export const TopBar = () => {
   return (
     <div className='top-bar-comp' >
+      <div className='top-bar-head' >
         Dashboard
+      </div>
+      <div classname='top-bar-profile' >
         Search
+        Bell
         Profile
+      </div>
+        
     </div>
   )
 }
