22565f98c71684ef7a1532de4aa0630b7b49b9ee
ankit-suman-07 <ankitsuman07@gmail.com>
2023-09-02 07:31:46 +0530

    Moddified style for topbar

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import "../css/TopBar.css";
+import Bell from "../icons/bell.png";
+import Search from "../icons/search.png";
+import Profile from "../icons/profile.jpg";
 
 export const TopBar = () => {
   return (
@@ -7,10 +10,17 @@ export const TopBar = () => {
       <div className='top-bar-head' >
         Dashboard
       </div>
-      <div classname='top-bar-profile' >
-        Search
-        Bell
-        Profile
+      <div className='top-bar-profile' >
+        <div className='search-bar' >
+          <input type='text' placeholder='Search' className='search-text' />
+          <img src={Search} className='search-icon' />
+        </div>
+        <div className='top-bar-bell' >
+          <img src={Bell} />
+        </div>
+        <div className='top-bar-id' >
+          <img src={Profile} />
+        </div>
       </div>
         
     </div>
