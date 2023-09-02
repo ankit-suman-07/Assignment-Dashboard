616e3b9e7b7d5d57c2721a95c66db9cd5a76455a
ankit-suman-07 <ankitsuman07@gmail.com>
2023-09-02 10:31:13 +0530

    Modified PieChart style, added several pages

--- src/components/PieChart.jsx
+++ src/components/PieChart.jsx
@@ -12,6 +12,7 @@ export const data = [
 export const options = {
   colors: ["#98D89E", "#EE8484", "F6DC7D"],
   legend: "none",
+  pieSliceText: "none", // Hide percentages on the pie chart
   // title: "Top Products",
 };
 
@@ -26,20 +27,47 @@ export const PieChart = () => {
           <span>Sort</span>
       </div>
       <div className='pie-chart-main' >
-      <Chart
-      chartType="PieChart"
-      data={data}
-      options={options}
-      width={"70%"}
-      height={"170px"}
-    />
+        <Chart
+          chartType="PieChart"
+          data={data}
+          options={options}
+          width={"70%"}
+          height={"170px"}
+        />
 
-      <div className='pie-chart-details' >
-        
+        <div className='pie-chart-details' >
+          <div className='pie-chartitem' >
+            <div className='pie-chart-inner' >
+              <div className='circle green' ></div> <span className='item-name' >Basic Tees</span>
+            </div>
+            <div className='pie-chart-inner' >
+              <div className='circle white' ></div> <span className='item-amount' >55%</span>
+            </div>
+            
+          </div>
+
+          <div className='pie-chartitem' >
+            <div className='pie-chart-inner' >
+              <div className='circle yellow' ></div> <span className='item-name' >Custom Short Pants</span>
+            </div>
+            <div className='pie-chart-inner' >
+              <div className='circle white' ></div> <span className='item-amount' >31%</span>
+            </div>
+            
+          </div>
+
+          <div className='pie-chartitem' >
+            <div className='pie-chart-inner' >
+              <div className='circle pink' ></div> <span className='item-name' >Super Hoodies</span>
+            </div>
+            <div className='pie-chart-inner' >
+              <div className='circle white' ></div> <span className='item-amount' >14%</span>
+            </div>
+            
+          </div>
+          
+        </div>
       </div>
-      </div>
-      
-    
     </div>
   );
 }
---
+++ src/pages/SchedulesPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const SchedulesPage = () => {
+  return (
+    <div>SchedulesPage</div>
+  )
+}
