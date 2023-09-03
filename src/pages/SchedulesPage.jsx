77755a36752a0907645435e6cd524804d529998e
ankit-suman-07 <ankitsuman07@gmail.com>
2023-09-03 21:44:38 +0530

    Updated SchedulesPage

--- src/pages/SchedulesPage.jsx
+++ src/pages/SchedulesPage.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import "../css/SchedulesPage.css";
 import { Sidebar } from '../components/Sidebar';
+import { ScheduleDB } from '../helper/ScheduleDB';
+import { ScheduleRow } from '../components/ScheduleRow';
+import { colors } from '../helper/ScheduleDB';
 
 export const SchedulesPage = () => {
   return (
     <div className='schedule-page-comp' >
         <Sidebar />
         <div className='schedule-page-div' >
-        <div className='top-bar-head' >
-          Schedules
-        </div>
+          <div className='top-bar-head' >
+            Schedules
+          </div>
+          <div className='schedule-list' >
+      {
+        ScheduleDB.map((schedule, idx) => {
+          return(
+             <ScheduleRow id={idx} schedule={schedule}  color={colors[idx%7]} />
+          );
+        })
+      }
+      </div>
         </div>
     </div>
   )
